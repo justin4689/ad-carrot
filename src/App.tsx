4d56943a4d3af17bdbb7e66de8340a9cdb6bde7d
justin <justintrah8@gmail.com>
2026-02-23 12:25:11 +0100

    pricing added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Services from "./components/Services";
 import ProjectShowCase from "./components/ProjectShowCase";
 import AdAgencyPackages from "./components/AdAgencyPackages";
+import Team from "./components/Team";
 function App() {
   return (
     <div className="overflow-y-hidden bg-orange-50 text-emerald-950 antialiased">
@@ -14,6 +15,7 @@ function App() {
         <Services />
         <ProjectShowCase/>
         <AdAgencyPackages />
+        <Team />
     </div>
   );
 }
