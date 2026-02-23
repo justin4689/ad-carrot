f492d9b3447b9afe4d253ce828872d0967f2dc19
justin <justintrah8@gmail.com>
2026-02-23 12:35:28 +0100

    team added

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,10 +1,24 @@
 
 
+import { TEAM_MEMBERS } from "../constants";
  function Team () {
     return(
 
-        <section>
-            
+        <section className="container mx-auto px-8 py-8" id="team">
+            <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">Team</h2>
+            <div className="flex flex-wrap">
+                {
+                    TEAM_MEMBERS.map((member, index) => (
+                        <div className="mb-12 flex w-full flex-col items-center px-4 text-center md:w-1/2 lg:w-1/3">
+                            <img src={member.image} alt={member.name} className="rounded-full w-32 h-32  mb-4" />
+                            <h3 className="md:text-xl font-semibold tracking-tighter">{member.name}</h3>
+                            <p className="italic md:text-lg">{member.title}</p>
+                        </div>
+                    ))
+                }
+
+            </div>
+
         </section>
     )
  }
